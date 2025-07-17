6d29bc608bcfc81f550863b2a213f4a45fd62caa
asshifasultana <asmonika82@gmail.com>
2025-07-17 21:09:10 +0600

    Update BadMole.java

--- BadMole.java
+++ BadMole.java
@@ -2,6 +2,7 @@ package Model;
 
 public class BadMole extends Mole {
 
+     protected ImageIcon badMole;
     public BadMole(int point, int visibleTime) {
         super(point, visibleTime);
         badMole = new ImageIcon("BadMole.png");
