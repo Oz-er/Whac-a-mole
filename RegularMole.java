a7b858970be7f352ac45efda41718d54698713f9
asshifasultana <asmonika82@gmail.com>
2025-07-17 21:09:55 +0600

    Update RegularMole.java

--- RegularMole.java
+++ RegularMole.java
@@ -1,9 +1,14 @@
 package Model;
 
+import javax.swing.*;
+
 public class RegularMole extends Mole{
+
+    protected ImageIcon moleIcon;
 
     public RegularMole(int point, int visibleTime) {
         super(point, visibleTime);
+        moleIcon = new ImageIcon("mole.png");
     }
 
     public int getPoint(){
