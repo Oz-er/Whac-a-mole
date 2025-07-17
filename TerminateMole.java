16a985036aa3e4d125eda8e3dac8a511a09acffc
asshifasultana <asmonika82@gmail.com>
2025-07-17 21:10:19 +0600

    Update TerminateMole.java

--- TerminateMole.java
+++ TerminateMole.java
@@ -1,9 +1,13 @@
 package Model;
 
+import javax.swing.*;
+
 public class TerminateMole extends Mole {
 
+    protected ImageIcon bombMole;
     public TerminateMole(int point, int visibleTime) {
         super(point, visibleTime);
+        bombMole = new ImageIcon("bomb.png");
     }
 
     public int getPoint() {
