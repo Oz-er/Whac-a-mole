60b74ca2e51cbc50868d3ff68e3b0a316cadc16a
asshifasultana <asmonika82@gmail.com>
2025-07-27 22:37:38 +0600

    Update MainMenuPanel.java

--- MainMenuPanel.java
+++ MainMenuPanel.java
@@ -5,47 +5,73 @@ import java.awt.*;
 
 public class MainMenuPanel extends JPanel {
     Image background;
+    Image button;
 
-    public MainMenuPanel() {
+    public MainMenuPanel(JPanel panel,CardLayout cardLayout) {
          background = new ImageIcon("D:/WhacAMole/src/View/MenuPanel.png").getImage();
+         button = new ImageIcon("D:/WhacAMole/src/View/button.png").getImage();
          setLayout(null);
 
+         //start Button
         JButton startButton = new JButton("Start Game");
         startButton.setFont(new Font("Tahoma", Font.BOLD + Font.ITALIC, 16));
         startButton.setBackground(new Color(150,75,0));
-        startButton.setSize(150,30);
-        startButton.setLocation(100,200);
-        startButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-        startButton.setAlignmentY(Component.CENTER_ALIGNMENT);
+        startButton.setSize(130,30);
+        startButton.setLocation(110,410);
+        startButton.setOpaque(false);
         startButton.setFocusable(false);
         startButton.setVisible(true);
+        startButton.setBorderPainted(false);
         add(startButton);
 
+        //High Score
+        JButton scoreButton = new JButton("Score");
+        scoreButton.setFont(new Font("Tahoma", Font.BOLD + Font.ITALIC, 16));
+        scoreButton.setBackground(new Color(150,75,0));
+        scoreButton.setSize(130,30);
+        scoreButton.setLocation(110,460);
+        scoreButton.setOpaque(false);
+        scoreButton.setFocusable(false);
+        scoreButton.setVisible(true);
+        scoreButton.setBorderPainted(false);
+        add(scoreButton);
+
+        //music button
         JButton musicButton = new JButton("Music");
         musicButton.setFont(new Font("Tahoma", Font.BOLD + Font.ITALIC, 16));
         musicButton.setBackground(new Color(150,75,0));
-        musicButton.setSize(150,30);
-        musicButton.setLocation(100,240);
+        musicButton.setSize(130,30);
+        musicButton.setLocation(110,510);
         musicButton.setFocusable(false);
+        musicButton.setOpaque(false);
+        musicButton.setBorderPainted(false);
         musicButton.setVisible(true);
+        musicButton.addActionListener(e -> cardLayout.show(panel, "MusicSettingPanel"));
         add(musicButton);
 
+        //about button
         JButton aboutButton = new JButton("About");
         aboutButton.setFont(new Font("Tahoma", Font.BOLD + Font.ITALIC, 16));
         aboutButton.setBackground(new Color(150,75,0));
-        aboutButton.setSize(150,30);
-        aboutButton.setLocation(100,280);
+        aboutButton.setSize(130,30);
+        aboutButton.setLocation(110,560);
         aboutButton.setFocusable(false);
+        aboutButton.setOpaque(false);
+        aboutButton.setBorderPainted(false);
         aboutButton.setVisible(true);
         add(aboutButton);
 
+        //exit button
         JButton exitButton = new JButton("Exit");
         exitButton.setFont(new Font("Tahoma", Font.BOLD + Font.ITALIC, 16));
         exitButton.setBackground(new Color(150,75,0));
-        exitButton.setSize(150,30);
-        exitButton.setLocation(100,320);
+        exitButton.setSize(130,30);
+        exitButton.setLocation(110,610);
         exitButton.setFocusable(false);
+        exitButton.setOpaque(false);
+        exitButton.setBorderPainted(false);
         exitButton.setVisible(true);
+        exitButton.addActionListener(e -> System.exit(0));
         add(exitButton);
         }
 
@@ -53,6 +79,11 @@ public class MainMenuPanel extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(background, 0, 0, getWidth(), getHeight(), this);
+        g.drawImage(button,100, 400, 150, 50, this);
+        g.drawImage(button,100, 450, 150, 50, this);
+        g.drawImage(button,100,500,150,50,this);
+        g.drawImage(button,100,550,150,50,this);
+        g.drawImage(button,100,600,150,50,this);
     }
 }
 
