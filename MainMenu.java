338c60ea9dcb9121ac07a4cd3809e315133438e2
asshifasultana <asmonika82@gmail.com>
2025-07-27 22:38:42 +0600

    Update MainMenu.java

--- MainMenu.java
+++ MainMenu.java
@@ -1,18 +1,27 @@
 package View;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainMenu {
-    public MainMenu() {
+    public static void main(String[] args) {
         JFrame frame = new JFrame("Whac-A-Mole");
         ImageIcon image = new ImageIcon("D:/WhacAMole/src/View/MenuPanel.png");
         frame.setIconImage(image.getImage());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 400);
+        frame.setSize(700, 800);
         frame.setLocationRelativeTo(null);
         frame.setLayout(null);
-        frame.setContentPane(new MainMenuPanel());
         frame.setResizable(false);
+
+        CardLayout cardLayout = new CardLayout();
+        JPanel panel = new JPanel(cardLayout);
+        panel.add(new MainMenuPanel(panel,cardLayout),"MainMenuPanel");
+        panel.add(new MusicSettingPanel(panel, cardLayout),"MusicSettingPanel");
+        frame.add(panel);
+
+        frame.setContentPane(panel);
+
 
         frame.setVisible(true);
     }
